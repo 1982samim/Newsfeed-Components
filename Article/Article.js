c738dd26d5acfd34c0176e0d2bbce58aec86e119
Sami Web28 <58615068+1982samim@users.noreply.github.com>
2020-02-13 19:26:33 -0800

    Toggle added

--- Article/Article.js
+++ Article/Article.js
@@ -3,7 +3,7 @@
 const data = [
   {
     title: 'Lambda School Students: "We\'re the best!"',
-    date: 'Nov 5th, 2018',
+    date: "Nov 5th, 2018",
     firstParagraph: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura skywalker lando
         moff wicket tatooine luke.Solo wampa wampa calrissian yoda moff.Darth grievous darth gonk darth hutt.Darth baba skywalker
         watto fett jango maul han.Mon ewok sidious sidious lando kenobi grievous gamorrean solo.Yoda wedge utapau darth calamari.
@@ -23,8 +23,8 @@ const data = [
         moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`
   },
   {
-    title: 'Javascript and You, ES6',
-    date: 'May 7th, 2019',
+    title: "Javascript and You, ES6",
+    date: "May 7th, 2019",
     firstParagraph: `Alohamora wand elf parchment, Wingardium Leviosa hippogriff, house dementors betrayal. Holly, Snape centaur portkey ghost
         Hermione spell bezoar Scabbers. Peruvian-Night-Powder werewolf, Dobby pear-tickle half-moon-glasses, Knight-Bus. Padfoot
         snargaluff seeker: Hagrid broomstick mischief managed. Snitch Fluffy rock-cake, 9 ¾ dress robes I must not tell lies. Mudbloods
@@ -43,8 +43,8 @@ const data = [
         sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.`
   },
   {
-    title: 'React vs Angular vs Vue',
-    date: 'June 7th, 2019',
+    title: "React vs Angular vs Vue",
+    date: "June 7th, 2019",
     firstParagraph: `Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ivysaur Lorem ipsum dolor sit amet, consectetur adipiscing
         elit. Venusaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charmander Lorem ipsum dolor sit amet, consectetur
         adipiscing elit. Charmeleon Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charizard Lorem ipsum dolor sit amet,
@@ -71,8 +71,8 @@ const data = [
         Castform Lotad the power that's inside Burnt Berry Makuhita. Ghost Ariados Corphish Dusclops Golbat Gligar Zweilous.`
   },
   {
-    title: 'Professional Software Development in 2019',
-    date: 'Jan 1st, 2019',
+    title: "Professional Software Development in 2019",
+    date: "Jan 1st, 2019",
     firstParagraph: `Hodor hodor HODOR! Hodor hodor - hodor, hodor. Hodor hodor... Hodor hodor hodor; hodor hodor. Hodor hodor hodor, hodor, hodor
           hodor. Hodor, hodor. Hodor. Hodor, hodor - hodor... Hodor hodor hodor; hodor HODOR hodor, hodor hodor?! Hodor hodor, hodor.
           Hodor hodor hodor hodor hodor! Hodor hodor - HODOR hodor, hodor hodor hodor hodor hodor; hodor hodor? `,
@@ -112,41 +112,50 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
-const art = document.querySelector('.articles');
+const art = document.querySelector(".articles");
 
 function articleTemplate(article, item) {
-// Creating & Appending Div
-  let div = document.createElement('div');
-      div.classList.add('article');
-      art.append(div);
+  // Creating & Appending Div
+  let div = document.createElement("div");
+  div.classList.add("article");
+  art.append(div);
   // art.append(div);
 
-// Creating & Appending H2
-  let title = document.createElement('h2');
-      title.textContent = article[item].title;
-      div.append(title);
+  // Creating & Appending H2
+  let title = document.createElement("h2");
+  title.textContent = article[item].title;
+  div.append(title);
 
-// Creating & Appending Date
-  let date = document.createElement('p');
-      date.classList.add('date');
-      date.textContent = article[item].date;
-      div.append(date);
+  // Creating & Appending Date
+  let date = document.createElement("p");
+  date.classList.add("date");
+  date.textContent = article[item].date;
+  div.append(date);
 
-// Creating & Appending Article Paragraphs
-  let p1 = document.createElement('p');
-      p1.textContent = article[item].firstParagraph;
-      div.append(p1);
+  let expandB = document.createElement("span");
+      expandB.textContent = 'Open Article'
+  expandB.addEventListener("click", () => {
+    div.classList.toggle("article-open");
+  });
+  div.append(expandB);
 
-  let p2 = document.createElement('p');
-      p2.textContent = article[item].secondParagraph;
-      div.append(p2);
+  // Creating & Appending Article Paragraphs
+  let p1 = document.createElement("p");
+  p1.textContent = article[item].firstParagraph;
+  div.append(p1);
 
-  let p3 = document.createElement('p');
-      p3.textContent = article[item].thirdParagraph;
-      div.append(p3);
+  let p2 = document.createElement("p");
+  p2.textContent = article[item].secondParagraph;
+  div.append(p2);
+
+  let p3 = document.createElement("p");
+  p3.textContent = article[item].thirdParagraph;
+  div.append(p3);
 }
 
 articleTemplate(data, 0);
 articleTemplate(data, 1);
 articleTemplate(data, 2);
 articleTemplate(data, 3);
+
+// Expand Button
